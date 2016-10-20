4908196f6c37a5853396818cbacb78d6ab21e068
Jason Kiraly <jaykiraly@gmail.com>
2016-10-20 23:29:20 +0000

    Handled null case of activeBook.

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { connect } from 'redux';
+import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if(!this.props.book) {
+      return <div>Select a book to get started.</div>
+    }
+    
     return (
       <div>
-        Book Detail
+        <h3>Details for:</h3>
+        <div>{this.props.book.title}</div>
       </div>
     );
   }
